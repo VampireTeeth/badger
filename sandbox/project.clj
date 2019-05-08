86a7b78c33a2f73df1a0eae1d1c85864b5287ec5
VampireTeeth <vampireteeth001@gmail.com>
2019-05-08 21:41:52 +1000

    Adding sandbox for playing around with yada

---
+++ sandbox/project.clj
@@ -0,0 +1,12 @@
+(defproject sandbox "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [yada "1.2.15"]
+                 [bidi "2.1.6"]
+                 [byte-streams "0.2.4"]]
+  :main ^:skip-aot sandbox.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
---
+++ sandbox/src/sandbox/core.clj
@@ -0,0 +1,30 @@
+(ns sandbox.core
+  (:require [yada.yada :as yada])
+  (:gen-class))
+
+;; Define a new variable web-server to hold the web-server
+;; created via yada/listener
+(def web-server)
+
+(defn- make-web-server
+  []
+  (yada/listener
+   ["/"
+    [
+     ["hello" (yada/as-resource "Hello World!")]
+     ["test" (yada/resource {:produces "text/plain"
+                        :response "This is a test!"})]
+     [true (yada/as-resource nil)]]]
+   {:port 3000}))
+
+(defn- new-web-server
+  [_]
+  ;; Creating a new web server
+  (make-web-server))
+
+
+(defn go
+  []
+  (when (and (resolve 'web-server) (bound? #'web-server))
+    ((:close web-server)))
+  (alter-var-root #'web-server new-web-server))
