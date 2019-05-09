caa5cfb6aa7467dd16fc5068828b6ad066c34a6b
Steven Liu <sliu2@atlassian.com>
2019-05-09 10:55:22 +1000

    More changes

--- sandbox/src/sandbox/core.clj
+++ sandbox/src/sandbox/core.clj
@@ -2,6 +2,33 @@
   (:require [yada.yada :as yada])
   (:gen-class))
 
+(defn- hello-to
+  [name]
+  {:message (str "Hello," name)})
+
+;; Definitions of resource
+(def default-404
+  {:produces
+   {:media-type "application/json" :charset "utf-8"}
+   :response
+   {:status 404 :message "Not Found"}})
+
+(def route-no-match-resource
+  {:methods
+   {:* default-404}
+   :properties {:exists? false}
+   :responses
+   {404 default-404}})
+
+
+(def hello-to-resource
+  (yada/resource
+   {:methods
+    {:get
+     {:produces
+      {:media-type "application/json" :charset "utf-8"}
+      :response (fn [ctx]
+                  (-> ctx :parameters :path :name hello-to))}}}))
 ;; Define a new variable web-server to hold the web-server
 ;; created via yada/listener
 (when (not (resolve 'web-server))
@@ -13,9 +40,11 @@
    ["/"
     [
      ["hello" (yada/as-resource "Hello World!")]
+     [["hello/" :name] hello-to-resource]
      ["test" (yada/resource {:produces "text/plain"
                              :response "This is a test!"})]
-     [true (yada/as-resource nil)]]]
+     [true (yada/resource route-no-match-resource)]]]
+
    {:port 3000}))
 
 (defn- new-web-server
